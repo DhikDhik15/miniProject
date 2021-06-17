19367b2706916ee1d864836ff24d6389eb23faef
DhikDhik15 <opor@gmail.com>
2021-06-17 13:59:36 +0700

    add >> product

---
+++ config/database/product.js
@@ -0,0 +1,18 @@
+// configurasi untuk sequilize
+'use strict';
+var dotevn = require('dotenv');
+dotevn.config();
+
+module.exports = {
+    HOST: process.env.HOST_MINIPROJECT,
+    USER: process.env.USER_MINIPROJECT,
+    PASSWORD: process.env.PASSWORD_MINIPROJECT,
+    DB: process.env.DB_MINIPROJECT_PRODUCT,
+    dialect: "postgres", //ini akan memakai query nya dari siapa ?
+    pool: { //ini seperti request time out / time limit misal ada error
+        max: 5,
+        min: 0,
+        acquire: 30000, //satuannya second
+        idle: 10000
+    }
+}
