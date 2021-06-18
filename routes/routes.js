3094b0939c45b87137f06a140d8dd7d331a5ce88
DhikDhik15 <opor@gmail.com>
2021-06-18 16:43:10 +0700

    add routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 module.exports = function (app) {
   var province = require('../controllers/services/Account/province');
+  var account = require('../controllers/services/Account/account');
 
   app.route('/province').post(province.addProvince);
   app.route('/province').get(province.getProvince);
   app.route('/province').put(province.putProvince);
   app.route('/province/:id').delete(province.deleteProvince);
+
+  app.route('/account').post(account.addAccount);
+  app.route('/account').get(account.getAccount);
 }
