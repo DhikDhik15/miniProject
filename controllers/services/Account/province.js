f1af7cc1779bb04b3ea5d778841c350443438d74
DhikDhik15 <opor@gmail.com>
2021-06-18 14:09:38 +0700

    POST

---
+++ controllers/services/Account/province.js
@@ -0,0 +1,50 @@
+var dbAccount = require('../../../models/account/index');
+var tableProvince = dbAccount.province;
+const sequelize = require('sequelize');
+
+// post
+
+exports.addProvince = (req, res) => {
+  const post = {
+    name: req.body.name
+  }
+
+  if (!post.name){
+    res.status(422).json({
+      message: 'Name empty'
+    })
+    return;
+  } else {
+    tableProvince.findAll({
+      attributes:['id', 'name'],
+      where: {
+        name:post.name
+      }
+    }).then((dataProvince) => {
+      if (dataProvince.length >= 1){
+        res.status(422).json({
+          message: 'Provinsi sudah ada',
+        })
+
+        /*POST DATA*/
+      } else {
+        tableProvince.create(post)
+        .then((data) => {
+          res.status(200).json({
+            message: 'Berhasil',
+            data:data
+          });
+        }).catch((err) => {
+          res.status(500).send({
+            message:err.message
+          });
+        })
+      }
+
+    }).catch((err) => {
+      res.status(500).json({
+        message: err
+      });
+    })
+  }
+}
