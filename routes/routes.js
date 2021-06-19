63bf903882f453d3370f497931a0ec87768a9775
sgibdd1 <sgibdd1@gmail.com>
2021-06-19 11:49:56 +0700

    update >> routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,9 @@ module.exports = function (app) {
   var account = require('../controllers/services/Account/account');
 
   app.route('/province').post(province.addProvince);
-  app.route('/province').get(province.getProvince);
-  app.route('/province').put(province.putProvince);
-  app.route('/province/:id').delete(province.deleteProvince);
+  app.route('/getProvince').get(province.getProvince);
+  app.route('/putProvince').put(province.putProvince);
+  app.route('/delProvince/').delete(province.deleteProvince);
 
   app.route('/account').post(account.addAccount);
   app.route('/account').get(account.getAccount);
