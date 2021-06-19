724252796bdd2d9f23240b7c10e33bb324c383b4
DhikDhik15 <opor@gmail.com>
2021-06-19 22:59:35 +0700

    update routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,12 @@
 module.exports = function (app) {
   var province = require('../controllers/services/Account/province');
   var account = require('../controllers/services/Account/account');
+  var auth = require('../controllers/services/Auth/users');
+
+  app.route('/').get(auth.getUser);
+  app.route('/login').post(auth.addUsers);
+  app.route('/admin').get(auth.getAdmin);
+  app.route('/logout').get(auth.logOut);
 
   app.route('/province').post(province.addProvince);
   app.route('/getProvince').get(province.getProvince);
