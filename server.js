dbee46928335cfd024dc0b6eaf9098e8a220b27f
DhikDhik15 <opor@gmail.com>
2021-06-21 12:00:26 +0700

    add config SSL

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require ('express-session');
+/*HTTPS*/ 
+const https = require ('https');
+const path = require ('path');
+const fs = require ('fs');
+/*REDIS*/ 
 const redis = require ('redis');
 const redisStore = require ('connect-redis')(session);
 const client = redis.createClient();
+/*.ENV*/ 
 const dotenv = require('dotenv');
+/*CORS*/ 
 const cors = require ("cors");
 const Sequelize = require("sequelize");
+/*SWAGGER*/ 
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
 const apiDocumentation = require('./miniProject.json');
 
-//connect to models
+/*connect to models*/
 dbAccount = require('./models/account/index');
 dbProduct = require('./models/product/index');
 dbTransaction = require('./models/transaction/index');
@@ -33,6 +41,22 @@ app.use(session({
   resave: false
 })); 
 /* end */
+
+/*HTTPS connection*/
+/*STEPS*/
+/*1. Generate a private key
+      openssl genrsa -out key.pem
+  2. Create CSR using private key
+      openssl req -new -key key.pem -out csr.epm
+      (rename extension file csr.epm become csr.pem)
+  3.  Generate SSL certification from CSR
+      openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out cert.pem */  
+const sslServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app)
+
+/*end connection*/  
 
 /*Connect to views */ 
 app.use(bodyParser.json());
@@ -65,6 +89,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+/*ROUTES*/ 
 const routes = require('./routes/routes');
 routes(app);
 
@@ -78,6 +103,6 @@ dbAuth.sequelize.sync();
 
 /*config PORT*/
 const PORT = process.env.PORT || 8001;
-app.listen(PORT, () => {
-  console.log(`Running On PORT ${PORT}.`);
+sslServer.listen(PORT, () => {
+  console.log(`Secure connection on PORT ${PORT}.`);
 });
