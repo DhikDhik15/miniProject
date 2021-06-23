c62a484fd0de685dca8098faf80b031facf85f84
DhikDhik15 <opor@gmail.com>
2021-06-23 15:43:54 +0700

    route

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,16 @@
+const { district } = require('../models/account');
+
+
 module.exports = function (app) {
   var province = require('../controllers/services/Account/province');
   var account = require('../controllers/services/Account/account');
   var auth = require('../controllers/services/Auth/users');
+  var district = require('../controllers/services/Account/district');
 
+  
   app.route('/').get(auth.getUser);
   app.route('/login').post(auth.addUsers);
+  app.route('/token').post(auth.tokenUsers);
   app.route('/admin').get(auth.getAdmin);
   app.route('/logout').get(auth.logOut);
 
@@ -17,4 +23,7 @@ module.exports = function (app) {
   app.route('/getAccount').get(account.getAccount);
   app.route('/putAccount').put(account.putAccount);
   app.route('/deleteAccount').delete(account.deleteAccount);
+
+  app.route('/district').post(district.addDistrict);
+  app.route('/getDistrict/:id').get(district.getDistrict);
 }
