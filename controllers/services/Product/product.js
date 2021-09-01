a1fc294fbb682c15074ab927a57a32b5b1e35a0b
DhikDhik15 <putraadhika15@gmail.com>
2021-09-01 08:44:37 +0700

    fixx add

--- controllers/services/Product/product.js
+++ controllers/services/Product/product.js
@@ -3,50 +3,20 @@ var tableProduct = dbProduct.product;
 
 
 /* POST */
-// exports.addProduct = (req,res) => {
-
-//     const storage = multer.diskStorage({
-//         destination: path.join('uploads'),
-          
-//         filename: function (req, file, cb) {
-//             cb(null, file.fieldname + '-' + Date.now() +
-//             path.extname(file.originalname));
-//         }
-//     });
-    
-//     const upload = multer({storage: storage}).single('image');
-
-//     const add = {
-//         name: req.body.name,
-//         image: req.file.path
-//     }  
-//     tableProduct.create(add)
-//     .then(data => {
-//         res.status(200).json({
-//             message: 'Berhasil',
-//             data: data
-//         });
-//     })
-//     .catch(err => {
-//         res.status(500).send({
-//             message: err.message
-//         });
-//     });
-
-// }
 exports.addProduct = async function (req, res){
+    console.log(req.file);
     try {
-        if (!req.body.name || req.body.images == undefined){
-            res.status(400).json({
-                message: 'Empty'
-            });
-            return;
-        }
         const add = {
             name: req.body.name,
-            images: req.body.filename
-        };
-        await tableProduct.findAll()
+            image: req.file.filename
+        }
+        if (!req.body.name || req.file.filename == undefined){
+            res.status(400).json({
+                message: 'Form Empty'
+            });
+            return;
+        } else {
+            await tableProduct.findAll()
         .then(data => {
             if (data.length >= 3) {
                 res.status(422).json({
@@ -61,11 +31,11 @@ exports.addProduct = async function (req, res){
                 })
             }
         })
+        }
     } catch (error) {
         console.log(error);
         res.status(500).send({
             message: 'Error'
         });
-        
     }
 }
