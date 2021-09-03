b2678b63bdb7e9bb8da8d4e7a4f9fc3c283d4a09
DhikDhik15 <putraadhika15@gmail.com>
2021-09-03 14:21:16 +0700

    complate CRUD category

--- controllers/services/Product/category.js
+++ controllers/services/Product/category.js
@@ -1,5 +1,5 @@
-var dbProduct = require('../../../models/product/index');
-tableCategory = dbProduct.category;
+const dbProduct = require('../../../models/product/index');
+const tableCategory = dbProduct.category;
 
 /* POST */ 
 exports.addCategory = (req, res) => {
@@ -19,4 +19,70 @@ exports.addCategory = (req, res) => {
             message: err.message
         });
     });
+}
+
+/*GET*/
+exports.getCategory = (req, res) => {
+    tableCategory.findAndCountAll({
+        attributes: ['id', 'name'],
+        order: [['id', 'ASC']]
+    }).then((data) => {
+        res.status(200).send({
+            data: data,
+            message: 'list category'
+        });
+    })
+}
+
+/*PUT*/
+exports.putCategory = async function (req, res){
+    try {
+        const put = {
+            id: req.body.id,
+            name: req.body.name,
+        }
+        tableCategory.update({
+            name: put.name
+        },{
+            where: {
+                id: put.id
+            }
+        }).then((data) => {
+            if (data == 1){
+                res.status(200).json({
+                    message: 'updated'
+                });
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: 'Error'
+        });
+    }
+}
+
+/*DELETE*/
+exports.deleteCategory = function (req, res){
+    const id = req.body.id;
+
+    tableCategory.destroy({
+        where: {id: id}
+    })
+    .then(data => {
+        if(data >=1){
+            res.status(200).json({
+                message: 'Deleted'
+            });
+        } else {
+            res.status(404).json({
+                message: 'Nothing to delete'
+            });
+        }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: error
+        });
+    })
 }
