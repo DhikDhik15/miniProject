a2b29d152e9ae4acc62a29f74b48380d3a190bed
DhikDhik15 <putraadhika15@gmail.com>
2021-09-20 14:16:44 +0700

    service mongo

---
+++ controllers/services/mongo/product.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const Product = require('../../../middleware/product');
+
+exports.createProduct = async (req, res) => {
+    console.log(req.file);
+    try {
+        const payload = {
+            name: req.body.name,
+            price: req.body.price,
+            image: req.file.filename
+        }
+        const product = await Product.createProduct({
+            ...payload
+        });
+        res.status(200).json({
+            status: true,
+            data: product,
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: err,
+            status: false,
+        })
+    }
+}
+exports.getProducts = async (req, res) => {
+    try {
+        const products = await Product.products();
+        res.status(200).json({
+            status: true,
+            data: products,
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: err,
+            status: false,
+        })
+    }
+}
+
+exports.getProductById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const productDetails = await Product.productById(id);
+        res.status(200).json({
+            status: true,
+            data: productDetails,
+        })
+    } catch (err) {
+        res.status(500).json({
+            status: false,
+            error: err
+        })
+    }
+}
+exports.removeProduct = async (req, res) => {
+    try {
+        const id = req.params.id
+        const productDetails = await Product.removeProduct(id)
+        res.status(200).json({
+            status: true,
+            data: productDetails,
+        })
+    } catch (err) {
+        res.status(500).json({
+            status: false,
+            error: err
+        })
+    }
+}
