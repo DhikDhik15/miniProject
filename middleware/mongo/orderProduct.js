65ddf08f8999cbca75ed79a005a77c747d4b65ca
DhikDhik15 <putraadhika15@gmail.com>
2021-09-23 13:56:28 +0700

    mongo middleware

---
+++ middleware/mongo/orderProduct.js
@@ -0,0 +1,14 @@
+'use strict';
+const orderProduct = require('../../models/product/mongo/orderProduct');
+
+exports.order = async () => {
+    const orders = await orderProduct.find().populate({
+        path: "supplier",
+        select: "name"
+    });;
+    return orders;
+};
+exports.addOrder = async add => {
+    const newOrder = await orderProduct.create(add);
+    return newOrder;
+};
